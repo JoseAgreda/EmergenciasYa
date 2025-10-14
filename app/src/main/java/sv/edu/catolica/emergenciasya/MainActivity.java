758d76c3717bfa63db8d37a4383492e649896f12
Jose Agreda <jose.agreda@aracaristudios.com>
2025-10-14 14:02:44 -0600

    Pantalla Principal /cambios

--- app/src/main/java/sv/edu/catolica/emergenciasya/MainActivity.java
+++ app/src/main/java/sv/edu/catolica/emergenciasya/MainActivity.java
@@ -1,4 +1,32 @@
 package sv.edu.catolica.emergenciasya;
 
-public class MainActivity {
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnOption1, btnOption2, btnOption3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnOption1 = findViewById(R.id.btnOption1);
+        btnOption2 = findViewById(R.id.btnOption2);
+        btnOption3 = findViewById(R.id.btnOption3);
+
+        btnOption1.setOnClickListener(v -> {
+            // TODO: Action for emergency call
+        });
+
+        btnOption2.setOnClickListener(v -> {
+            // TODO: Open First Aid screen
+        });
+
+        btnOption3.setOnClickListener(v -> {
+            // TODO: Open Trusted Contacts
+        });
+    }
 }
